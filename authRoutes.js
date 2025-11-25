a7ee0eb3d635e3665e2172c8a91dbf0457d923cd
yxs-site <tutyxe@gmail.com>
2025-11-25 02:19:11 -0300

    Add files via upload

---
+++ authRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+// Rota de Registro
+router.post('/register', authController.register);
+
+// Rota de Login
+router.post('/login', authController.login);
+
+// Rota para solicitar recuperação de senha
+router.post('/forgot-password', authController.forgotPassword);
+
+// Rota para redefinir a senha
+router.post('/reset-password', authController.resetPassword);
+
+// Rota para alterar a senha (protegida)
+router.post('/change-password', authMiddleware, authController.changePassword);
+
+// Rota para excluir a conta (protegida)
+router.delete('/delete-account', authMiddleware, authController.deleteAccount);
+
+module.exports = router;
